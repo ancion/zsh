020c2d77b55d823adda802bf9bfa0b1fda1f0c20
ancion <ancion@LAPTOP-KFSOTC2E.localdomain>
2020-06-30 21:19:43 +0800

    delete some unnecessay config

---
+++ env.zsh
@@ -0,0 +1,21 @@
+export XDG_CONFIG_HOME=$HOME/.config
+export LOCALBIN=$XDG_CONFIG_HOME/bin
+export PATH=$PATH:$LOCALBIN
+export PATH=$PATH:$HOME/.gem/ruby/2.6.0
+export PATH=$PATH:/usr/local/bin
+export PATH=$PATH:$HOME/.local/bin
+export TERM=xterm-256color
+export TERM_ITALICS=true
+export PATH=$PATH:/home/linuxbrew/.linuxbrew/bin
+export RANGER_LOAD_DEFAULT_RC="false"
+export EDITOR=nvim
+export ZSH_AUTOSUGGEST_USE_ASYNC=1
+export ZSH_AUTOSUGGEST_MANUAL_REBIND=1
+
+# If you come from bash you might have to change your $PATH.
+ export PATH=$HOME/bin:/usr/local/bin:$PATH
+JAVA_HOME=/usr/local/src/jdk1.8.0_51
+JAVA_BIN=/usr/local/src/jdk1.8.0_51/bin
+PATH=$JAVA_HOME/bin:$PATH
+CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar
+export JAVA_HOME JAVA_BIN PATH CLASSPATH
